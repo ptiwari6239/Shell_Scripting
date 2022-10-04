cf4c9b5171749c612d6e017e2acf4198f1ab72d5
ptiwari <princetiwari97940@gmail.com>
2022-10-04 21:58:30 +0530

    funtion script

---
+++ funtion_01.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###########################
+# start
+##########################
+
+
+function scriptname {   
+	echo Hello this is a function
+}
+
+
+scriptname #function call
+
+########################
+# END
+#########################
---
+++ try.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo try something on my own 
+
+./while_loop.sh
+
+echo done 
