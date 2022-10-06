cce7f493377923d94f426dd7a2a8add672d4fbf1
ptiwari <princetiwari97940@gmail.com>
2022-10-06 14:20:10 +0530

    arguments in shell sripting

---
+++ args_demo.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+echo total number of arguments are $#
+
+echo first arguments is $1
+
+echo all arguments are $@ 
+
+echo end of file 
+
---
+++ dev_null.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# /dev/null
+# in some case if we don't wanna print the output of a command
+# on terminal and also write a file 
+# we can redirect the output to /dev/null
+# examples 
+
+
+cd /root/ &> /dev/null
+
+
