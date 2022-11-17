ca259f7448302599f662060f113f2bf4a496df21
anjaneya <princetiwari97940@gmail.com>
2022-11-17 21:20:31 +0530

    updated tranferfile

--- transferfile.sh
+++ transferfile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# this bash script move files from one directory to another directory 
+# it only move files which are only have .zip extension 
+
 cd /home/ptiwari/Downloads/
 
 mv *.zip /home/ptiwari/Desktop/SERVER_FILE/JAVA
